6fa15d5775fcde6f9aab22ad6a6ba9e85d87ca74
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-05-22 19:17:11 +0300

    add swiper to Result Page

--- src/components/pages/resultPage/ResultPage.tsx
+++ src/components/pages/resultPage/ResultPage.tsx
@@ -1,6 +1,6 @@
-import React, { FC, JSX, RefObject, useRef } from 'react';
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import { FC, JSX } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, FreeMode, Keyboard, Mousewheel, Pagination } from "swiper/modules";
 import PageLayout from '../../pageLayout';
 import Header from '../../header';
 import BackLink from '../../backLink';
@@ -9,6 +9,13 @@ import { useAppDispatch, useAppSelector, useSettings } from "../../../features/h
 import { selectEquations, clearEquations_multiplication } from "../../../store/equationsSlice";
 import locales from "../../../features/locales";
 import type { Solution_Multiplication } from "../../../types";
+import SWIPER_PARAMS from "../../../utils/swiper-params";
+import 'swiper/scss';
+import 'swiper/scss/effect-coverflow';
+import 'swiper/scss/free-mode';
+import 'swiper/scss/keyboard';
+import 'swiper/scss/mousewheel';
+import 'swiper/scss/pagination';
 import styles from "./ResultPage.module.scss";
 
 const ResultPage: FC = (): JSX.Element => {
@@ -17,47 +24,55 @@ const ResultPage: FC = (): JSX.Element => {
 	const leftTab: JSX.Element = (
 		<BackLink
 			to='/select-multiplier'
-			alt='link to multiplication table'
-			onClick={() => {
-				dispatch(clearEquations_multiplication());
-			}}
+			alt='link to select a multiplier'
+			onClick={() => {dispatch(
+				clearEquations_multiplication()
+			);}}
 		/>
 	);
 
 	const { language } = useSettings();
 	const headerTitle: string = locales[language].resultTitle;
 
-	const animationRef: RefObject<HTMLOListElement | null> = useRef<HTMLOListElement>(null);
-
-	useGSAP(() => {
-		gsap.from("li", {
-			scale: 0,
-			opacity: 0,
-			stagger: 0.05
-		});
-	}, { scope: animationRef });
-
 	const { multiplication } = useAppSelector(selectEquations);
-	const solutionsList: Solution_Multiplication[] | [] = multiplication.solutions;
-	const mainContent: JSX.Element = (
-		<article className={styles._}>
-			<ol ref={animationRef}>
-				{solutionsList.map(({
-					subjectOfRepetition,
-					secondMultiplier,
-					product,
-				}: Solution_Multiplication): JSX.Element => (
-					<li key={secondMultiplier}>
+	const list: Solution_Multiplication[] | [] = multiplication.solutions;
+
+	const solutionsList: JSX.Element = (
+		<div className={styles.swiperContainer}>
+			<Swiper
+				{...SWIPER_PARAMS.MULTIPLICATION_TABLE}
+				modules={[
+					EffectCoverflow,
+					FreeMode,
+					Keyboard,
+					Mousewheel,
+					Pagination,
+				]}
+			>
+				{list.map(({
+				  subjectOfRepetition,
+				  secondMultiplier,
+				  product
+			  }: Solution_Multiplication): JSX.Element => (
+					<SwiperSlide key={secondMultiplier}>
 						<MultiplicationExample
 							firstMultiplier={subjectOfRepetition}
 							secondMultiplier={secondMultiplier}
 							hideResult={false}
 							userAnswer={product}
 						/>
-					</li>
+					</SwiperSlide>
 				))}
-			</ol>
-		</article>
+			</Swiper>
+		</div>
+	);
+
+	const mainContent: JSX.Element = (
+		<section className={styles._}>
+			<article>
+				{solutionsList}
+			</article>
+		</section>
 	);
 
 	return (
--- src/utils/swiper-params/index.ts
+++ src/utils/swiper-params/index.ts
@@ -4,6 +4,7 @@ import { multiplicationTablePageSwiperParams } from "./multiplication-table-page
 const SWIPER_PARAMS = {
 	SELECT_MULTIPLIER: selectMultiplierPageSwiperParams,
 	MULTIPLICATION_TABLE: multiplicationTablePageSwiperParams,
+	RESULT_LIST: multiplicationTablePageSwiperParams,
 };
 
 export default SWIPER_PARAMS;
