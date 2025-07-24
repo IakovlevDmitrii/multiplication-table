7f66bd87e1f6c4d27a7d7c23eb6a3b078e0f7a27
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-07-24 22:52:19 +0300

    create Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { FC, JSX } from "react";
+import "./Footer.module.scss";
+
+const Footer: FC = (): JSX.Element => (
+	<footer>
+		<p>&copy; 2025 ID</p>
+	</footer>
+);
+
+export default Footer;
