9a3d90fc1a9062b01b5d1eb540f427496d13149a
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2025-07-26 00:21:27 +0300

    delete SwiperMultiplierList component

--- src/components/pages/selectMultiplierPage/multplierList/MultiplierList.tsx
+++ src/components/pages/selectMultiplierPage/multplierList/MultiplierList.tsx
@@ -1,12 +1,67 @@
 import { FC, JSX, useMemo } from "react";
-import SwiperMultiplierList from "../swiperMultiplierList/SwiperMultiplierList";
+import { useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+	Autoplay,
+	EffectCoverflow,
+	Keyboard,
+	Mousewheel,
+	Navigation,
+	Pagination
+} from "swiper/modules";
+import MultiplierItem from "../multiplierItem/MultiplierItem";
+import SWIPER_PARAMS from "../../../../utils/swiper-params";
+import { useAppDispatch } from "../../../../hooks";
+import { setTargetMultiplierAction } from "../../../../store/equations/equationsSlice";
 import { createDefaultArrayRange } from "../../../../utils/math";
+import "swiper/scss";
+import "swiper/scss/autoplay";
+import "swiper/scss/effect-coverflow";
+import "swiper/scss/keyboard";
+import "swiper/scss/mousewheel";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import styles from "./MultiplierList.module.scss";
 
 const MultiplierList: FC = (): JSX.Element => {
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 	const multiplierList: number[] = useMemo((): number[] => createDefaultArrayRange(), []);
 
+	const onMultiplierClick = (newMultiplier: number): void => {
+		dispatch(setTargetMultiplierAction(newMultiplier));
+		navigate(`/multiplication-table/${newMultiplier}`);
+	};
+
 	return (
-		<SwiperMultiplierList multiplierList={multiplierList} />
+		<div className={styles._}>
+			<Swiper
+				{...SWIPER_PARAMS.SELECT_MULTIPLIER}
+				modules={[
+					Autoplay,
+					EffectCoverflow,
+					Keyboard,
+					Mousewheel,
+					Navigation,
+					Pagination,
+				]}
+			>
+				{multiplierList.map(
+					(multiplier: number): JSX.Element => (
+						<SwiperSlide key={multiplier}>
+							<MultiplierItem
+								multiplier={multiplier}
+								onMultiplierClick={(multiplier: number) => onMultiplierClick(multiplier)}
+							/>
+						</SwiperSlide>
+					)
+				)}
+			</Swiper>
+			<div className={styles.navigation}>
+				<div className="button-prev"></div>
+				<div className="button-next"></div>
+			</div>
+		</div>
 	);
 };
 
